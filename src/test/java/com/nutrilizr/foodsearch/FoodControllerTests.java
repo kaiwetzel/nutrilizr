86a28796640a7a5cd9534292a2e2971a1bddb9c1
Kai <ainvar@nickname.berlin.de>
2022-01-17 01:25:52 +0100

    Add basic test for the food controller, closes #5

---
+++ src/test/java/com/nutrilizr/foodsearch/FoodControllerTests.java
@@ -0,0 +1,43 @@
+package com.nutrilizr.foodsearch;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.nutrilizr.foodsearch.controllers.FoodController;
+
+@WebMvcTest(FoodController.class)
+public class FoodControllerTests {
+	@Autowired 
+	private MockMvc mvc;
+	
+	/**
+	 * Basic test to check if the food search endpoint is working correctly, connecting to the
+	 * Fat Secrets server successfully and providing plausible results.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	void searchFoodItems() throws Exception {
+		// Since only the controller is tested, no "/api" prefix is used for the requested URL:
+		final ResultActions result = mvc.perform(get("/food/search")
+			.queryParam("query", "pasta")
+			.queryParam("page", "1") // 2nd page; should be OK as long as there is enough pasta.
+			.contentType(MediaType.APPLICATION_JSON));
+		
+		result
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$").isArray())
+			.andExpect(jsonPath("$.length()", is(50)));
+	}
+}
